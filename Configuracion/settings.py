301997ee4eede8310c35df3b30d5de17bcbe51c2
Luis Sarango <djgatomixx24@gmail.com>
2019-10-02 08:01:42 -0500

    Estable sin esquemas compartidos

--- Configuracion/settings.py
+++ Configuracion/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'prueba'
+    'prueba',
     'bootstrap4',
     'import_export',
     'storage',
